297f97599ac287a1623d34f84d6914751f220cdf
Luis Schuimer <85784931+LuisSchuimer@users.noreply.github.com>
2022-11-05 14:03:40 +0100

    Added Dictionary to Backend (Version 2)

--- backend/backend.py
+++ backend/backend.py
@@ -9,20 +9,39 @@ def get_data(location):
     URL = f"https://api.waqi.info/feed/{location}/?token={TOKEN}"
     print("Getting Data...")
     response = requests.get(URL)
-    data = response.text
-    get_data = json.loads(data)
+    response_data = response.text
+    get_data = json.loads(response_data)
     print(get_data)
     print("Loading Data...")
     try:
+        pm25 = None
+        pm10 = None
         air_quality_index = get_data['data']['aqi']
         station_name = get_data['data']['city']['name']
         station_id = get_data['data']['idx']
         last_update = get_data['data']['time']['s']
-        if 'pm25' in get_data.keys():
+        try:
+            # Emission von Feinstaub der Patikelgröße PM2.5
             pm25 = get_data['data']['iaqi']['pm25']['v']
-        if 'pm10' in get_data.keys():
+        except:
+            pass
+        try:
+            # Emission von Feinstaub der Patikelgröße PM10
             pm10 = get_data['data']['iaqi']['pm10']['v']
-        check_aqi(air_quality_index)
+        except:
+            pass
+        index = check_aqi(air_quality_index)
+        data = {
+            "AQI": air_quality_index,
+            "Air Quality": index,
+            "Station Name": station_name,
+            "Station ID": station_id,
+            "Last Update": last_update,
+            "PM2.5": pm25,
+            "PM10": pm10
+
+        }
+        print(data)
     except TypeError:
         status = get_data['status']
         context = get_data['data']
@@ -44,4 +63,4 @@ def check_aqi(aqi):
     return index
 
 
-
+get_data("Berlin")
