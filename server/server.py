2b0ae0e9311a2cc2c844b4faff678abf6939d5b0
Luis Schuimer <85784931+LuisSchuimer@users.noreply.github.com>
2023-01-15 20:14:37 +0100

    Bug fixes

--- server/server.py
+++ server/server.py
@@ -90,24 +90,25 @@ def lueften():
 
 @app.route('/luft')
 def luft():
-    country = request.args.get('country')
-    luft = backend.get_data(country, "Server")
-    print(luft)
-    requests = luft["Requests"]
-    Stationsname = luft['Station Name']
-    Updatestatus = luft['Last Update']
-    quality = luft['Air Quality']
-    forecast_1_avg_pm25 = luft['Forecast PM2.5 Avg']
-    forecast_1_avg_pm10 = luft['Forecast PM10 Avg']
+    try:
+        country = request.args.get('country')
+        luft = backend.get_data(country, "Server")
+        print(luft)
+        requests = luft["Requests"]
+        Stationsname = luft['Station Name']
+        Updatestatus = luft['Last Update']
+        quality = luft['Air Quality']
+        forecast_1_avg_pm25 = luft['Forecast PM2.5 Avg']
+        forecast_1_avg_pm10 = luft['Forecast PM10 Avg']
 
-    max_scale, scale, index, forecast_day1, aqi_avg, pm25_avg, no2_avg, pm10_avg = backend.scale_germany(luft)
+        max_scale, scale, index, forecast_day1, aqi_avg, pm25_avg, no2_avg, pm10_avg = backend.scale_germany(luft)
 
-    return render_template('daten.html', Stationsname=Stationsname, Updatestatus=Updatestatus, quality=quality,
-                           lueften=lueften, max_scale=max_scale, scale=scale, index=index,
-                           forecast_day1_index=forecast_day1, forecast_1_avg_pm25=forecast_1_avg_pm25,
-                           forecast_1_avg_pm10=forecast_1_avg_pm10, requests=requests, country=country)
-
-
+        return render_template('daten.html', Stationsname=Stationsname, Updatestatus=Updatestatus, quality=quality,
+                               lueften=lueften, max_scale=max_scale, scale=scale, index=index,
+                               forecast_day1_index=forecast_day1, forecast_1_avg_pm25=forecast_1_avg_pm25,
+                               forecast_1_avg_pm10=forecast_1_avg_pm10, requests=requests, country=country)
+    except:
+        return render_template("main.html", Error=True)
 if __name__ == '__main__':
     backend.average_germany("AQI")
     backend.average_germany("PM10")
